2511bd36615b049afbc466872a23546f21541984
Ryan Berlin <ryanaberlin@gmail.com>
2018-10-09 20:06:29 -0400

    added hyperlinks to runners on manage recruits

--- manage_script.js
+++ manage_script.js
@@ -10,14 +10,13 @@ function unfollow(){
 	var checkboxes = document.getElementsByTagName("input");
 	for (var i = 0; i < checkboxes.length; i++){
 		if(checkboxes[i].checked){
-			chrome.storage.local.remove(checkboxes[i].value);
+			chrome.storage.local.remove(checkboxes[i].parentElement.value);
 			var lineitem = checkboxes[i].parentElement;
 			var list = document.getElementById("runnersList");
 			list.removeChild(lineitem);
 			i--;
 		}
 	}
-
 }
 
 function selectAll(){
@@ -35,7 +34,15 @@ function displayRunners(data){
 		var name = data[ids[i]]["name"];
 		var list = document.getElementById("runnersList");
 		var runner = document.createElement("li");
-		runner.innerHTML = "<input type=\'checkbox\' value=\'" + ids[i] + "\'> "  + name;
+		runner.setAttribute("value", ids[i]);
+		runner.innerHTML = "<input type=\'checkbox\'><a href=\'\'> " + name + "</a>";
 		list.appendChild(runner);
+		runner.getElementsByTagName("a")[0].addEventListener("click", link);
 	}
+}
+
+function link(e){
+	//var newURL = "http://pa.milesplit.com/athletes/pro/2911907/stats";
+	var newURL = "http://milesplit.com/athletes/" + e.target.parentElement.value + "/stats";
+	chrome.tabs.update({url:newURL});
 }
